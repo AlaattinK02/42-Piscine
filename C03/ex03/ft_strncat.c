5d034f13418c2c2622b5a7fb778bc16bb1372b61
AlaattinK02 <33603926+AlaattinK02@users.noreply.github.com>
2021-10-24 15:17:51 +0300

    Updated

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akoral <akoral@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/24 15:15:03 by akoral            #+#    #+#             */
+/*   Updated: 2021/10/24 15:15:03 by akoral           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+char *ft_strncat(char *dest, char *src, unsigned int nb)
+{
+    int	i;
+	int	srci;
+
+	i = 0;
+	srci = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	i++;
+	while (src[srci] != '\0')
+	{
+		dest[i] = src[srci];
+		i++;
+		srci++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
