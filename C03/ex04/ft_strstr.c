a4342686459619371f60691401d264747013808d
AlaattinK02 <33603926+AlaattinK02@users.noreply.github.com>
2021-10-24 18:25:15 +0300

    Update

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -21,16 +21,21 @@ char *ft_strstr(char *str, char *to_find)
 
 	i = 0;
 	if (to_find[0] == '\0')
+    {
 		return (str);
+    }
 	while (str[i] != '\0')
 	{
 		f = 0;
 		while (str[i + f] != '\0' && str[i + f] == to_find[f])
 		{
 			if (to_find[f + 1] == '\0')
+            {
 				return (&str[i]);
+            }
 			++f;
 		}
 		++i;
 	}
 	return (0);
+}
